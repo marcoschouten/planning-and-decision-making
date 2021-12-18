bd14b36671f601c8b9b4318d3d1eff3e9ebec151
SailorBrandon <shaohang.han@outlook.com>
2021-12-19 00:14:39 +0100

    modified dwa

--- Quadrotor/quadsim.py
+++ Quadrotor/quadsim.py
@@ -6,35 +6,36 @@ from .quadrotor import Quadrotor
 
 
 class QuadSim:
-    def __init__(self,controller,des_state,Tmax,update,plan,seg_num,
-                 pos = None, attitude = [0,0,0],
-                 animation_frequency = 50,
-                 control_frequency = 200):
+    def __init__(self, controller, des_state, Tmax, update, plan, seg_num,
+                 pos=None, attitude=[0, 0, 0],
+                 animation_frequency=50,
+                 control_frequency=200):
 
         self.t = 0
         self.Tmax = Tmax
         self.dt = 1/control_frequency
         self.animation_rate = 1/animation_frequency
         self.control_iterations = int(control_frequency / animation_frequency)
-        self.update=update
-        self.plan=plan
-        self.segnum=seg_num
+        self.update = update
+        self.plan = plan
+        self.segnum = seg_num
         self.des_state = des_state
         self.controller = controller
-        if pos is None: pos = des_state(0).pos
+        if pos is None:
+            pos = des_state(0).pos
         self.Quadrotor = Quadrotor(pos, attitude)
-        
+
         self.pos_history = deque(maxlen=100)
 
     def Step(self):
         des_state = self.des_state(self.t)
         state = self.Quadrotor.get_state()
         state_dot = self.Quadrotor.statedot
-        segnum=self.segnum(self.t)
-        self.update(des_state,state,segnum,state_dot)
+        segnum = self.segnum(self.t)
+        self.update(des_state, state, segnum, state_dot)
         local_des_state = self.plan()
         if(self.t >= self.Tmax):
-            U, M = self.controller.run_hover(state, local_des_state,self.dt)
+            U, M = self.controller.run_hover(state, local_des_state, self.dt)
         else:
             U, M = self.controller.run(state, local_des_state)
         self.Quadrotor.update(self.dt, U, M)
@@ -45,36 +46,36 @@ class QuadSim:
             self.Step()
         return self.Quadrotor.world_frame()
 
-    def run(self,ax = None,save = False):
+    def run(self, ax=None, save=False):
         self.init_plot(ax)
         while self.t < self.Tmax + 2:
             frame = self.control_loop()
             self.update_plot(frame)
             plt.pause(self.animation_rate)
 
-    def init_plot(self,ax = None):
+    def init_plot(self, ax=None):
         if ax is None:
             fig = plt.figure()
             ax = Axes3D.Axes3D(fig)
-            ax.set_xlim((0,2))
-            ax.set_ylim((0,2))
-            ax.set_zlim((0,2))
-        ax.plot([], [], [], '-', c='red',zorder = 10)
-        ax.plot([], [], [], '-', c='blue',zorder = 10)
-        ax.plot([], [], [], '-', c='green', marker='o', markevery=2,zorder = 10)
-        ax.plot([], [], [], '.', c='green', markersize=2,zorder = 10)
+            ax.set_xlim((0, 2))
+            ax.set_ylim((0, 2))
+            ax.set_zlim((0, 2))
+        ax.plot([], [], [], '-', c='red', zorder=10)
+        ax.plot([], [], [], '-', c='blue', zorder=10)
+        ax.plot([], [], [], '-', c='green', marker='o', markevery=2, zorder=10)
+        ax.plot([], [], [], '.', c='green', markersize=2, zorder=10)
         self.lines = ax.get_lines()[-4:]
 
-    def update_plot(self,frame):
+    def update_plot(self, frame):
 
-        lines_data = [frame[:,[0,2]], frame[:,[1,3]], frame[:,[4,5]]]
+        lines_data = [frame[:, [0, 2]], frame[:, [1, 3]], frame[:, [4, 5]]]
 
         for line, line_data in zip(self.lines[:3], lines_data):
             x, y, z = line_data
             line.set_data(x, y)
             line.set_3d_properties(z)
 
-        self.pos_history.append(frame[:,4])
+        self.pos_history.append(frame[:, 4])
         history = np.array(self.pos_history)
-        self.lines[-1].set_data(history[:,0], history[:,1])
-        self.lines[-1].set_3d_properties(history[:,-1])
+        self.lines[-1].set_data(history[:, 0], history[:, 1])
+        self.lines[-1].set_3d_properties(history[:, -1])
--- runsim.py
+++ runsim.py
@@ -3,8 +3,7 @@ import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d.axes3d as Axes3D
 
 from PathPlanning import RRTStar, Map
-# from TrajGen import trajGenerator, Helix_waypoints, Circle_waypoints
-from TrajGen import trajOpt, Helix_waypoints, Circle_waypoints,dwa_planner
+from TrajGen import trajOpt, Helix_waypoints, Circle_waypoints, dwa_planner, trajGenerator
 from Quadrotor import QuadSim
 import controller
 np.random.seed(8)
@@ -18,48 +17,49 @@ obstacles = [[-5, 25, 0, 20, 35, 60],
              [70, 50, 0, 80, 80, 100]]
 
 # limits on map dimensions
-bounds = np.array([0,100])
+bounds = np.array([0, 100])
 # create map with obstacles
-mapobs = Map(obstacles, bounds, dim = 3)
+mapobs = Map(obstacles, bounds, dim=3)
 
-#plan a path from start to goal
-start = np.array([80,20,10])
-goal = np.array([30,80,80])
+# plan a path from start to goal
+start = np.array([80, 20, 10])
+goal = np.array([30, 80, 80])
 # start = np.array([80,20,10])
 # goal = np.array([60,20,10])
 
-rrt = RRTStar(start = start, goal = goal,
-              Map = mapobs, max_iter = 500,
-              goal_sample_rate = 0.1)
+rrt = RRTStar(start=start, goal=goal,
+              Map=mapobs, max_iter=500,
+              goal_sample_rate=0.1)
 
 waypoints, min_cost = rrt.plan()
 
 
-#scale the waypoints to real dimensions
+# scale the waypoints to real dimensions
 waypoints = 0.02*waypoints
-print(waypoints)
-#Generate trajectory through waypoints
+
+# Generate trajectory through waypoints
 # traj = trajGenerator(waypoints,max_vel = 5,gamma = 1e6)
-traj = trajOpt(waypoints,max_vel = 5,mean_vel = 0.8)
-#initialise simulation with given controller and trajectory
+traj = trajOpt(waypoints, max_vel=5, mean_vel=0.8)
+# initialise simulation with given controller and trajectory
 Tmax = traj.time_list[-1]
 des_state = traj.get_des_state
-seg_num= traj.get_seg_num
-local_planner=dwa_planner(des_state(0),des_state(0),mapobs,goal,waypoints)
-update=local_planner.update
-plan=local_planner.plan
-sim = QuadSim(controller,des_state,Tmax,update,plan,seg_num)
+seg_num = traj.get_seg_num
+local_planner = dwa_planner(
+    des_state(0), des_state(0), mapobs, goal, waypoints)
+update = local_planner.update
+plan = local_planner.plan
+sim = QuadSim(controller, des_state, Tmax, update, plan, seg_num)
 
-#create a figure
+# create a figure
 fig = plt.figure()
 ax = Axes3D.Axes3D(fig)
-ax.set_xlim((0,2))
-ax.set_ylim((0,2))
-ax.set_zlim((0,2))
+ax.set_xlim((0, 2))
+ax.set_ylim((0, 2))
+ax.set_zlim((0, 2))
 
-#plot the waypoints and obstacles
+# plot the waypoints and obstacles
 rrt.draw_path(ax, waypoints)
-mapobs.plotobs(ax, scale = 0.02)
+mapobs.plotobs(ax, scale=0.02)
 
-#run simulation
+# run simulation
 sim.run(ax)
